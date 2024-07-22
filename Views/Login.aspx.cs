086081ca747e00ceb70629362bb3ef2969da5e53
jenriquez <jenriquez@coopsanantonio.com>
2024-07-22 16:58:40 -0500

    Login:EncriptaDesencripta

--- Views/Login.aspx.cs
+++ Views/Login.aspx.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace MonitorJudicial.Views
 {
@@ -13,5 +19,150 @@ namespace MonitorJudicial.Views
         {
 
         }
+
+        public static string GenerateAESKey(int keySize)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = keySize;
+                aes.GenerateKey();
+                return Convert.ToBase64String(aes.Key);
+            }
+        }
+
+        private static readonly string EncryptionKey = GenerateAESKey(256);
+        //private static readonly string EncryptionKey = "l8hYQ6IY5FkFzRL9u7XvhTjzGBkeVwBjx+X/zXNL8Do="; // Clave generada
+
+        public static string EncryptString(string plainText)
+        {
+            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] keyBytes = Convert.FromBase64String(EncryptionKey);
+
+            if (keyBytes.Length != 16 && keyBytes.Length != 24 && keyBytes.Length != 32)
+            {
+                throw new ArgumentException("La clave especificada no tiene un tamaño válido para este algoritmo.");
+            }
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+                aes.IV = new byte[16]; // Vector de inicialización de 16 bytes
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(plainBytes, 0, plainBytes.Length);
+                        cs.Close();
+                    }
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+        public static string DecryptString(string cipherText)
+        {
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            byte[] keyBytes = Convert.FromBase64String(EncryptionKey);
+
+            if (keyBytes.Length != 16 && keyBytes.Length != 24 && keyBytes.Length != 32)
+            {
+                throw new ArgumentException("La clave especificada no tiene un tamaño válido para este algoritmo.");
+            }
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+                aes.IV = new byte[16]; // Vector de inicialización de 16 bytes
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+        }
+
+        public void InsertarNuevoUsuario(string usuario, string password, string email, string nombres, string apellidos, int rol, string codigoAbogado)
+        {
+            string encryptedPassword = EncryptString(password);
+            string connectionString = ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString;
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO [FBS_Respaldo_DC_Produccion].[FBS_SEGURIDADES].[USUARIO_ABOGADOS] " +
+                               "([CODIGOUSUARIO], [CLAVE], [EMAIL], [NOMBRES], [APELLIDOS], [ROL], [FECHA_CREACION], [ESTADO_ACTIVO], [CODIGOABOGADO]) " +
+                               "VALUES (@usuario, @password, @correo, @nombres, @apellidos, @rol, @fechaCreacion, @estadoActivo, @codigoAbogado)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@usuario", usuario);
+                    command.Parameters.AddWithValue("@password", encryptedPassword);
+                    command.Parameters.AddWithValue("@correo", email);
+                    command.Parameters.AddWithValue("@nombres", nombres);
+                    command.Parameters.AddWithValue("@apellidos", apellidos);
+                    command.Parameters.AddWithValue("@rol", rol);
+                    command.Parameters.AddWithValue("@fechaCreacion", DateTime.Now);
+                    command.Parameters.AddWithValue("@estadoActivo", true); // O el valor que consideres adecuado
+                    command.Parameters.AddWithValue("@codigoAbogado", codigoAbogado);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string password = txtPassword.Text;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString;
+
+            if (ValidarUsuario(usuario, password, connectionString))
+            {
+                Response.Redirect("/Default.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('Usuario o contraseña incorrectos');</script>");
+            }
+        }
+
+        private bool ValidarUsuario(string usuario, string password, string connectionString)
+        {
+            bool isValid = false;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT [CLAVE] FROM [FBS_Respaldo_DC_Produccion].[FBS_SEGURIDADES].[USUARIO_ABOGADOS] WHERE [CODIGOUSUARIO] = @usuario";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@usuario", usuario);
+
+                    connection.Open();
+                    string encryptedPassword = (string)command.ExecuteScalar();
+
+                    if (!string.IsNullOrEmpty(encryptedPassword))
+                    {
+                        string decryptedPassword = DecryptString(encryptedPassword);
+
+                        if (decryptedPassword == password)
+                        {
+                            isValid = true;
+                        }
+                    }
+                }
+            }
+
+            return isValid;
+        }
+
+
     }
 }
--- Views/Login.aspx.designer.cs
+++ Views/Login.aspx.designer.cs
@@ -13,5 +13,68 @@ namespace MonitorJudicial.Views
 
     public partial class Login
     {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuario;
+
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+
+        /// <summary>
+        /// Control chkRememberMe.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkRememberMe;
+
+        /// <summary>
+        /// Control lblRememberMe.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblRememberMe;
+
+        /// <summary>
+        /// Control btnIngresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIngresar;
+
+        /// <summary>
+        /// Control lnkForgotPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkForgotPassword;
     }
 }
